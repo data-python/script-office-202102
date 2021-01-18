c6ae0b3b87986f5d12260d97864cf876a94af175
luo0412 <1095847440@qq.com>
2021-01-18 16:48:58 +0800

    [](add)

--- _book/python-let-excel-fly-202007/04/案例08 按条件将一个工作表拆分为多个工作簿/按时间将课程安排表拆分为多个表格@nice.py
+++ _book/python-let-excel-fly-202007/04/案例08 按条件将一个工作表拆分为多个工作簿/按时间将课程安排表拆分为多个表格@nice.py
@@ -5,79 +5,83 @@ import tablib
 import xlwings as xw
 import pandas as pd
 
+
+
+# 1) 打开Excel
 file_path = './教师课程安排.xlsx'
 sheet_name = 'Sheet2'
-
 app = xw.App(visible = True, add_book = False)
 workbook = app.books.open(file_path)
 worksheet = workbook.sheets[sheet_name]
-data = dict()
-
 
 # info = worksheet.used_range
 # nrows = info.last_cell.row
 # ncols = info.last_cell.column
 
+# 2) 使用pandas读取数据与索引
+data = dict()
 df = pd.read_excel('教师课程安排.xlsx'
                    ,sheet_name="Sheet2"
                    ,index_col=[0,1])
-
 df.reset_index()
 # print(df)
 
+# 将数据重构后输出到工作表
 new_worksheet = workbook.sheets.add("输出")
 new_worksheet['A1'].value = df
 print(df)
 
-# workbook.save()
 
+# 3) 遍历数据, 按照日期+教师名称构建嵌套字典
 value = workbook.sheets["输出"].range('A3').expand('table').value
 for i in range(len(value)):
     date_name = value[i][1]
+    # 如果没有这个键值, 初始化为字典
     if date_name not in data:
         data[date_name] = dict()
 
     teacher_name = value[i][0]
+    # 如果没有这个键值, 初始化为数组
     if teacher_name not in data[date_name]:
         data[date_name][teacher_name] = []
     data[date_name][teacher_name].append(value[i])
 
+# 格式化输出得到的嵌套字典
 pp = pprint.PrettyPrinter(compact=True)
 pp.pprint(data)
 
-
-workbook.sheets.add("教师课程安排-转置表")
-i = 0
+# 4) 用tablib临时输出Excel
 list = tablib.Dataset()
-
 for key,value in data.items():
     print(key)
     list.append(("日期: " + key, None,None,None))
+    # 加入表头
     list.append(('Time', 'Student',	'TA', 'Classrooms'))
 
     for key2, value2 in value.items():
         print(key2)
         list.append(('教师: ' + key2, None, None, None))
         for index in range(0, len(value2)):
-            # @todo xlwings目前导出一直会报错
+            # @todo xlwings目前导出一直会报错，先使用tablib临时输出Excel
             # print(value2[index][2:])
             # new_worksheet2.range('A' + str(i)).expand('table').value = value2[index][2:]
             # new_worksheet2[f'A{i}'].options(index = False).value = [1,32]
             # workbook.sheets['教师课程安排-转置表'].value = value2[index][2:]
-            i = i+1
             tup = tuple(value2[index][2:])
             print(tup)
             list.append(tup)
 
-        i = i+1
-    # 空行
+    # 加一行空行
     list.append((None, None, None, None))
 
 
-with open('教师课程安排-转置表.xlsx', 'wb') as f:#exl是二进制数据
+# 输出excel
+with open('教师课程安排-转置表.xlsx', 'wb') as f: #exl是二进制数据
     f.write(list.export('xlsx'))
 
+# 保存与关闭
 # workbook.save()
 workbook.close()
 
+# 退出
 app.quit()
