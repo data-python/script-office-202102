6b427be8810e40580da3ad260141645d2806974c
luo0412 <1095847440@qq.com>
2021-01-18 13:28:25 +0800

    [](add)

--- _book/python-let-excel-fly-202007/04/案例08 按条件将一个工作表拆分为多个工作簿/按时间将课程安排表拆分为多个表格@nice.py
+++ _book/python-let-excel-fly-202007/04/案例08 按条件将一个工作表拆分为多个工作簿/按时间将课程安排表拆分为多个表格@nice.py
@@ -1,6 +1,7 @@
 # https://segmentfault.com/a/1190000018597501
 import pprint
 
+import tablib
 import xlwings as xw
 import pandas as pd
 
@@ -52,7 +53,7 @@ for i in range(len(value)):
         data[date_name][teacher_name] = []
     data[date_name][teacher_name].append(value[i])
 
-pp = pprint.PrettyPrinter(width=41, compact=True)
+pp = pprint.PrettyPrinter(compact=True)
 pp.pprint(data)
 
 
@@ -61,20 +62,34 @@ pp.pprint(data)
 
 workbook.sheets.add("教师课程安排-转置表")
 i = 0
+list = tablib.Dataset()
+
 for key,value in data.items():
-    # print(value)
+    print(key)
+    list.append(("日期: " + key, None,None,None))
+    list.append(('Time', 'Student',	'TA', 'Classrooms'))
+
+
     for key2, value2 in value.items():
-        # print(value2)
+        print(key2)
+        list.append(('教师: ' + key2, None, None, None))
         for index in range(0, len(value2)):
-            print(value2[index][2:])
+            # print(value2[index][2:])
             # new_worksheet2.range('A' + str(i)).expand('table').value = value2[index][2:]
             # new_worksheet2[f'A{i}'].options(index = False).value = [1,32]
             # workbook.sheets['教师课程安排-转置表'].value = value2[index][2:]
             i = i+1
+            tup = tuple(value2[index][2:])
+            print(tup)
+            list.append(tup)
 
         i = i+1
+    # 空行
+    list.append((None, None, None, None))
 
-# workbook.save()
+
+with open('教师课程安排-转置表.xlsx', 'wb') as f:#exl是二进制数据
+    f.write(list.export('xlsx'))
 
 # workbook.save()
 workbook.close()
